71e3d11e600be19168ffa5cee09f4d16ac3e6707
Sam Varghese <svarghese@walmartlabs.com>
2014-10-11 19:22:06 -0700

    Adding the Files for form post

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,15 +12,27 @@ class TasksController < ApplicationController
     name = params[:name]
     description = params[:description]
     weightage = params[:weightage] || 5
+    if (due_date = params[:due_date])
+      if due_date == 'today'
+        due_date = Time.now()
+      elsif due_date == 'tomorrow'
+        due_date == Time.now() + 1.day
+      else due_date == 'later'
+        due_date = Time.now() + 5.days
+      end
+    else
+      due_date = Time.now() + 5.days
+    end
     status = 'active'
     if params[:employee_id]
       employee_id = params[:employee_id]
       Task.create(name: name, description: description, status: status,
-                  employee_id: employee_id.to_i, weightage: weightage.to_i)
+                  employee_id: employee_id.to_i, weightage: weightage.to_i,
+                  due_date: due_date)
       TaskDispatcher.new(employee.to_i).resort
     else
       Task.create(name: name, description: description, status: status,
-                  weightage: weightage.to_i)
+                  weightage: weightage.to_i, due_date: due_date)
     end
     head 200
   end
@@ -40,6 +52,18 @@ class TasksController < ApplicationController
     task.name = params[:name] if params[:name]
     task.description = params[:description] if params[:description]
     task.status = params[:status] if params[:status]
+    if (due_date = params[:due_date])
+      if due_date == 'today'
+        due_date = Time.now()
+      elsif due_date == 'tomorrow'
+        due_date == Time.now() + 1.day
+      else due_date == 'later'
+        due_date = Time.now() + 5.days
+      end
+      task.due_date = due_date
+    else
+      task.due_date = Time.now() + 5.days
+    end
     task.weightage = params[:weightage].to_i if params[:weightage]
     task.employee_id = params[:employee_id].to_i if params[:employee_id]
     task.save
